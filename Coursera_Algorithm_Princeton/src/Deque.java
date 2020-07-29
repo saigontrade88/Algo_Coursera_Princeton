3c26de6792c526114b1fe6a877defa1b1eb43a5b
saigontrade88 <longdangvn88@gmail.com>
2020-07-29 11:58:28 -0400

    Create a generic data type Deque that implements the provided API

---
+++ Coursera_Algorithm_Princeton/src/Deque.java
@@ -0,0 +1,54 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+	
+	// construct an empty deque
+    public Deque() {
+    	
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+    	return false;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+    	return 0;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+    	
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+    	
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+    	return null;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+    	return null;
+    }
+
+    // return an iterator over items in order from front to back
+   	@Override
+   	public Iterator<Item> iterator() {
+   		// TODO Auto-generated method stub
+   		return null;
+   	}
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+}
